89c47a2918c46f120f6f67d9338725952a9b2a21
jeshua19a <201801651g@utea.edu.pe>
2022-05-04 20:06:12 -0500

    modificando index con php

---
+++ pages/index.php
@@ -0,0 +1,73 @@
+<?php
+	session_start();
+	if(isset($_SESSION['user'])){
+		header('location:home.php');
+	}
+?>
+
+<?php include('header.php'); ?>
+<body>
+<div class="container">
+	<div style="height:50px;">
+	</div>
+    <div class="row" id="loginform">
+        <div class="col-md-4 col-md-offset-4">
+            <div class="login-panel panel panel-primary">
+                <div class="panel-heading">
+                    <h3 class="panel-title"><span class="glyphicon glyphicon-lock"></span> Iniciar Sesión
+                    	<span class="pull-right"><span class="glyphicon glyphicon-pencil"></span> <a style="text-decoration:none; cursor:pointer; color:white;" id="signup">Registrarse</a></span>
+                    </h3>
+                </div>
+            	<div class="panel-body">
+                	<form role="form" id="logform">
+                    	<fieldset>
+                        	<div class="form-group">
+                            	<input class="form-control" placeholder="Nombre" name="username" id="username" type="text" autofocus>
+                        	</div>
+                        	<div class="form-group">
+                            	<input class="form-control" placeholder="Contraseña" name="password" id="password" type="password">
+                        	</div>
+                        	<button type="button" id="loginbutton" class="btn btn-lg btn-primary btn-block"><span class="glyphicon glyphicon-log-in"></span> <span id="logtext">Iniciar Sesión</span></button>
+                    	</fieldset>
+                	</form>
+            	</div>
+            </div>
+        </div>
+    </div>
+
+    <div class="row" id="signupform" style="display:none;">
+        <div class="col-md-4 col-md-offset-4">
+            <div class="login-panel panel panel-primary">
+                <div class="panel-heading">
+                    <h3 class="panel-title"><span class="glyphicon glyphicon-pencil"></span> Registrarse
+                    	<span class="pull-right"><span class="glyphicon glyphicon-log-in"></span> <a style="text-decoration:none; cursor:pointer; color:white;" id="login">Iniciar Sesión</a></span>
+                    </h3>
+                </div>
+            	<div class="panel-body">
+                	<form role="form" id="signform">
+                    	<fieldset>
+                        	<div class="form-group">
+                            	<input class="form-control" placeholder="Nombre" name="susername" id="susername" type="text" autofocus>
+                        	</div>
+                        	<div class="form-group">
+                            	<input class="form-control" placeholder="Contraseña" name="spassword" id="spassword" type="password">
+                        	</div>
+                        	<button type="button" id="signupbutton" class="btn btn-lg btn-primary btn-block"><span class="glyphicon glyphicon-check"></span> <span id="signtext">Registrarse</span></button>
+                    	</fieldset>
+                	</form>
+            	</div>
+            </div>
+        </div>
+    </div>
+
+    <div id="myalert" style="display:none;">
+    	<div class="col-md-4 col-md-offset-4">
+    		<div class="alert alert-info">
+    			<center><span id="alerttext"></span></center>
+    		</div>
+   	 	</div>
+    </div>
+</div>
+<script src="custom.js"></script>
+</body>
+</html>

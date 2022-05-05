cfd0a0ea5b2049ef387c3032fa016bf254053c69
jeshua19a <201801651g@utea.edu.pe>
2022-05-04 20:31:23 -0500

    validando registro

--- pages/home.php
+++ pages/home.php
@@ -8,7 +8,33 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
   
     <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css">
+    <body>
+<div class="container">
+	<div style="height:50px;">
+	</div>
+    <div class="row">
+        <div class="col-md-4 col-md-offset-4">
+           <h2>Bienvenido, <?php echo $user; ?>!</h2>
+           <a href="logout.php" class="btn btn-danger"><span class="glyphicon glyphicon-log-out"></span> Cerrar Sesion</a>
+        </div>
+        <div class="wrapper">
+      <div class="task-input">
+        <img src="bars-icon.svg" alt="icon">
+        <input type="text" placeholder="Agregar Tareas">
+      </div>
+      <div class="controls">
+        <div class="filters">
+          <span class="active" id="all">Tareas</span>
+          <span id="pending">Pendiente</span>
+          <span id="completed">Completados</span>
+        </div>
+        <button class="clear-btn">Borrar Todo</button>
+      </div>
+      <ul class="task-box"></ul>
+    </div>
 
+    <script src="script.js"></script>
+    </div>
 </div>
 </body>
 </html>

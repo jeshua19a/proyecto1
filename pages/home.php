d485abbf88206e07c3a0493cbe683aaaa390db07
jeshua19a <201801651g@utea.edu.pe>
2022-05-04 20:26:23 -0500

    validacion del registro

---
+++ pages/home.php
@@ -0,0 +1,14 @@
+<?php include('session.php'); ?>
+<?php include('header.php'); ?>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">  
+    <title>Todo List </title>
+    <link rel="stylesheet" href="style1.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  
+    <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css">
+
+</div>
+</body>
+</html>
